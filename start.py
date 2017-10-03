377127823fc508b43e7c52a58c6d98bb3302cabd
srivollet <srivollet@objectif-informatique.fr>
2017-10-04 01:00:17 +0200

    test lib beautifulsoup4

--- start.py
+++ start.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python2.7
 
-#import the library used to query a website
-import urllib2
-
 #specify the url
-wiki = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
 
-#Query the website and return the html to the variable 'page'
-page = urllib2.urlopen(wiki)
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
 
 #import the Beautiful soup functions to parse the data returned from the website
 from bs4 import BeautifulSoup
 
 #Parse the html in the 'page' variable, and store it in Beautiful Soup format
-soup = BeautifulSoup(page)
+soup = BeautifulSoup(html_doc, 'html.parser')
 
-print soup.prettify()
+for link in soup.find_all('a'):
+    print(link.get('href'))
