5550aa706408a01ad3460abc706194e29f4c5942
srivollet <srivollet@objectif-informatique.fr>
2017-10-05 00:23:56 +0200

    lecture des tournois dans flash result

--- start.py
+++ start.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python2.7
-
-#specify the url
-html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
-
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
-
-<p class="story">...</p>
-"""
-
-#import the Beautiful soup functions to parse the data returned from the website
+import urllib2
 from bs4 import BeautifulSoup
 
-#Parse the html in the 'page' variable, and store it in Beautiful Soup format
-soup = BeautifulSoup(html_doc, 'html.parser')
+#specify the url
+url_flashresultat = "'http://www.bloomberg.com/quote/SPX:IND'"
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+#page = urllib2.urlopen(url_flashresultat)
+
+#soup = BeautifulSoup(page, 'html.parser')
+
+#print(soup);
+#print(soup.find_all('span', { 'class' : 'country_part' }))
+
+#import webbrowser
+
+#webbrowser.open('https://www.lequipe.fr/Football/ligue-1-resultats.html')
+
+from selenium import webdriver
+import re
+
+#browser = webdriver.Chrome()
+browser = webdriver.PhantomJS()
+
+browser.get('http://www.flashresultats.fr/')
+
+#On recupere le tableau des resultats
+resultats = browser.find_element_by_class_name('table-main')
+
+soup = BeautifulSoup(resultats.get_attribute('innerHTML'), 'html.parser')
+
+
+
+for tag in soup.find_all("span", class_="tournament_part"):
+    print(tag.text)
+
+browser.close()
