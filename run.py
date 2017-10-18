a26d4707e14708a16a9a9ed70116aad9f8a59bb3
srivollet <srivollet@objectif-informatique.fr>
2017-10-18 23:55:14 +0200

    mise à jour globale

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-import re, time, csv, os
+import re, time, csv, os, sqlite3
 from datetime import datetime
 
 browser = webdriver.Chrome()
@@ -38,17 +38,26 @@ for soccer in soup.find_all("table", class_="soccer odds"):
     if not exist:
         c.writerow(["date", "horaire","team_home","team_away","score","cote1","coteN","cote2"])
 
+    #db = sqlite3.connect('db/betapp.db')
+    #cursor = db.cursor()
+
     #On extrait chaque match
     for match in soccer.find('tbody').find_all('tr'):
         horaire = match.find('td', class_="cell_ad").text.encode('utf-8')
         team_home = match.find('td', class_="team-home").text.encode('utf-8')
-        team_away = match.find('td', class_="team-away").text.encode('utf-8')
+        team_visitor = match.find('td', class_="team-away").text.encode('utf-8')
         score = match.find('td', class_="score").text.encode('utf-8')
         cote1 = match.find('td', class_="cell_oa").text.encode('utf-8')
         coteN = match.find('td', class_="cell_ob").text.encode('utf-8')
         cote2 = match.find('td', class_="cell_oc").text.encode('utf-8')
 
         #insertion dans la base
-        c.writerow([datetime.now().strftime("%d%m%Y"), horaire,team_home,team_away,score,cote1,coteN,cote2])
+        c.writerow([datetime.now().strftime("%d%m%Y"), horaire,team_home,team_visitor,score,cote1,coteN,cote2])
+        #cursor.execute("INSERT INTO TEAMS(name,country) VALUES(?,?)", (str(team_home), 'null'))
+        #cursor.execute("INSERT INTO TEAMS(name,country) VALUES(?,?)", (str(team_visitor), 'null'))
+
+#        cursor.execute("INSERT INTO MATCHS(date,heure,score_home, score_visitor, team_home, team_visitor) VALUES(?,?,?,?,?,?)", 
+#            (datetime.now().strftime("%d%m%Y"), horaire, score_visitor, 0, team_home, team_visitor))
+
 
 browser.close()
