cc9a3029e1a91e07c3f9e7bc686908717a0ac86d
srivollet <srivollet@objectif-informatique.fr>
2017-10-21 12:10:29 +0200

    ajout du model

---
+++ model.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+class TypeCompetition(Enum):
+    CHAMPIONNAT = 0
+    COUPE = 1
+    AMICAL = 2
+
+class Pays:
+    nom = ""
+    continent = ""
+    def __str__(self):
+         return "nom= %s continent=%s" % (self.nom, self.continent)
+
+class Competition:
+    nom = ""
+    pays = Pays()
+    type = Enum
+    def __str__(self):
+         return "nom=%s pays=%s type=%s" % (self.nom, self.pays, self.type) 
+
+class Equipe:
+    nom = ""
+    pays = ""
+    def __str__(self):
+         return "foo"
+
+class Match:
+    date = ""
+    equipe_domicile = Equipe()
+    score_domicile = 0
+    equipe_visiteur = Equipe()
+    score_visiteur = 0
+    competition = Competition()
+    def __str__(self):
+         return "foo"
+
---
+++ rundb.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python2.7
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import re, time, csv, os, sqlite3
+from datetime import datetime
+from db import saveTeams
+
+browser = webdriver.Chrome()
+
+#On ouvre le site
+browser.get('http://www.flashresultats.fr/')
+
+#On va sur la page des cotes
+resultats = browser.find_element_by_css_selector('.ifmenu-odds.li4').click()
+
+time.sleep(3)
+
+#On recupere le tableau des resultats
+resultats = browser.find_element_by_class_name('odds-content')
+
+#On extrait le HTML pour traitement
+soup = BeautifulSoup(resultats.get_attribute('innerHTML'), 'html.parser')
+
+for soccer in soup.find_all("table", class_="soccer odds"):
+
+    #On extrait la competition
+    tournament_part = soccer.find("span", class_="tournament_part").text
+
+    exist = False
+
+    #Ouverture du fichier / creation du fichier
+    if os.path.exists("data/" + tournament_part + ".csv"):
+        exist = True
+
+    c = csv.writer(open("data/" + tournament_part + ".csv", "ab"))
+
+    if not exist:
+        c.writerow(["date", "horaire","team_home","team_away","score","cote1","coteN","cote2"])
+
+    #db = sqlite3.connect('db/betapp.db')
+    #cursor = db.cursor()
+
+    #On extrait chaque match
+    for match in soccer.find('tbody').find_all('tr'):
+        horaire = match.find('td', class_="cell_ad").text.encode('utf-8')
+        team_home = match.find('td', class_="team-home").text.encode('utf-8')
+        team_visitor = match.find('td', class_="team-away").text.encode('utf-8')
+        score = match.find('td', class_="score").text.encode('utf-8')
+        cote1 = match.find('td', class_="cell_oa").text.encode('utf-8')
+        coteN = match.find('td', class_="cell_ob").text.encode('utf-8')
+        cote2 = match.find('td', class_="cell_oc").text.encode('utf-8')
+
+        #insertion dans la base
+        c.writerow([datetime.now().strftime("%d%m%Y"), horaire,team_home,team_visitor,score,cote1,coteN,cote2])
+        #cursor.execute("INSERT INTO TEAMS(name,country) VALUES(?,?)", (str(team_visitor), 'null'))
+        #saveTeams(cursor, team_home)
+#        cursor.execute("INSERT INTO MATCHS(date,heure,score_home, score_visitor, team_home, team_visitor) VALUES(?,?,?,?,?,?)", 
+#            (datetime.now().strftime("%d%m%Y"), horaire, score_visitor, 0, team_home, team_visitor))
+
+
+browser.close()
+    
